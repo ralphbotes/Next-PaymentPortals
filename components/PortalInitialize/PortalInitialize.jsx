d557ecf32546d4b8466c3d60de37cdd20243f590
ralphbotes <ralphbotes@gmail.com>
2024-05-21 21:29:17 +0200

    Start implementing paygate request

--- components/PortalInitialize/PortalInitialize.jsx
+++ components/PortalInitialize/PortalInitialize.jsx
@@ -1,15 +1,38 @@
 import CodeDisplay from '../CodeDisplay/CodeDisplay';
 import styles from './PortalInitialize.module.css';
 import data from '../../data/data';
+import { Button } from '@mui/material';
+import initiate_paygate from "../../pages/api/initiate_paygate";
+import { useState } from 'react';
 
 export default function PortalInitialize({portal}) {
+    const [initiatePayRequest, setInitiatePayRequest] = useState({});
 
     const sampleCode = data[portal.name].initialize.code
     const sampleLanguage = data[portal.name].initialize.language
 
+    const handle_request = async () => {
+        if (portal.name === "paygate") {
+            const request = await initiate_paygate();
+            setInitiatePayRequest(request)
+        }
+    }
+
     return (
         <>
             <CodeDisplay code={sampleCode} language={sampleLanguage} />
+            {
+                !initiatePayRequest ?
+                    <Button
+                        variant="contained"
+                        onClick={() => handle_request()}
+                        sx={{ mt: 1, mr: 1 }}
+                    >
+                        Initiate transaction request
+                    </Button>
+                :
+                <CodeDisplay code={initiatePayRequest} language={"javascript"} />
+            }
         </>
     )
 }
