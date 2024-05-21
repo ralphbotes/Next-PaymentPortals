56c582bf076e35e46137b2b9427a0a7d7f1a0217
ralphbotes <ralphbotes@gmail.com>
2024-05-21 21:29:04 +0200

    initiate paygate rename and modifications final

---
+++ pages/api/initiate_paygate.js
@@ -0,0 +1,49 @@
+import CryptoJS from 'crypto-js';
+
+export default async function initiate_paygate() {
+  // Encryption key
+  const encryptionKey = 'secret';
+
+  // Create the data object
+  const data = {
+      PAYGATE_ID: '10011072130',
+      REFERENCE: 'pgtest_123456789',
+      AMOUNT: '3299',
+      CURRENCY: 'ZAR',
+      RETURN_URL: 'https://my.return.url/page',
+      TRANSACTION_DATE: new Date().toISOString().slice(0, 19).replace('T', ' '),
+      LOCALE: 'en-za',
+      COUNTRY: 'ZAF',
+      EMAIL: 'customer@paygate.co.za'
+  };
+
+  // Create checksum
+  const checksumString = Object.values(data).join('') + encryptionKey;
+  const checksum = CryptoJS.MD5(checksumString).toString();
+
+  // Add checksum to the data
+  data.CHECKSUM = checksum;
+
+  // Convert data to URL-encoded string
+  const fieldsString = new URLSearchParams(data).toString();
+
+  // Define the request options
+  const requestOptions = {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Referer': window.location.hostname
+      },
+      body: fieldsString
+  };
+
+  try {
+    // Send the POST request
+    const res = await fetch('https://secure.paygate.co.za/payweb3/initiate.trans', requestOptions);
+    if (res.ok) {
+        return await res.text();
+    }
+  } catch (error) {
+      return { "Error": "An error occurred during the request" };
+  }
+}
