49873419d3e63805487e20a7349513ca78027075
ralphbotes <ralphbotes@gmail.com>
2024-05-22 21:37:15 +0200

    Form redirect func added

---
+++ libs/FormRedirect.jsx
@@ -0,0 +1,24 @@
+export default function FormRedirect(a_data) {
+    // Create a hidden form element
+    const form = document.createElement('form');
+    form.style.display = 'none'; // Hide the form
+
+    // Set form attributes
+    form.method = 'POST';
+    form.action = a_data["submit_url"];
+
+    // Add form fields
+    for (item in a_data["form_list"]) {
+        const requestIdInput = document.createElement('input');
+        requestIdInput.type = 'hidden';
+        requestIdInput.name = item["key"];
+        requestIdInput.value = item["value"];
+        form.appendChild(requestIdInput);   
+    }
+
+    // Append the form to the document body
+    document.body.appendChild(form);
+
+    // Submit the form
+    form.submit();
+}
