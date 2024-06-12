c2205197cfa163f1d44d711f87b9d8061afa3b8e
ralphbotes <ralphbotes@gmail.com>
2024-06-12 22:47:11 +0200

    Add: new utils for paygate and unique ref

--- utils/common_utils.js
+++ utils/common_utils.js
@@ -1,3 +1,5 @@
+import CryptoJS from 'crypto-js';
+
 export const portalEnums = require("../public/data/payment_enums.json");
 
 export const project_data = require("../public/data/data.json");
@@ -15,3 +17,32 @@ export function getPortalData(a_portal) {
 export function capitalize(str) {
     return str.charAt(0).toUpperCase() + str.slice(1);
 }
+
+export function paygate_checksum(a_data,a_encryptionKey) {
+    const checksumString = Object.values(a_data).join('') + a_encryptionKey;
+    return CryptoJS.MD5(checksumString).toString();
+}
+
+export function parsePaygateData(a_data) {
+    const keyValuePairs = a_data.split('&');
+
+    const result = {};
+    keyValuePairs.forEach(pair => {
+        const [key, value] = pair.split('=');
+        result[key] = value;
+    });
+    
+    return result;
+}
+
+export function generateReference(length = 10) {
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let reference = '';
+    
+    for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      reference += characters[randomIndex];
+    }
+    
+    return reference;
+  }
