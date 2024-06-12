2b4930ef47165636c4f8b35557f4322c37592247
ralphbotes <ralphbotes@gmail.com>
2024-06-12 22:45:29 +0200

    config update

--- next.config.js
+++ next.config.js
@@ -3,7 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
   output: "export",
   env: {
-    NEXT_PUBLIC_HOST: process.env.NEXT_PUBLIC_HOST
+    NEXT_PUBLIC_HOST: process.env.NEXT_PUBLIC_HOST,
+    NEXT_PUBLIC_NODE_ENV: process.env.NEXT_PUBLIC_NODE_ENV,
+    NEXT_PUBLIC_PAYGATE_SECRET: process.env.NEXT_PUBLIC_PAYGATE_SECRET,
+    NEXT_PUBLIC_PAYGATE_MERCHANT: process.env.NEXT_PUBLIC_PAYGATE_MERCHANT
   }
 }
 
